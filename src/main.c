a51f8cacc9f620bcf6b6267545a35cc6a490e997
TheGraveyardWarden <ali.manutd10r@gmail.com>
2024-11-22 20:09:00 +0300

    better option handling

--- src/main.c
+++ src/main.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 #include "stepmgr.h"
 #include "rand_str.h"
 
+enum command {
+	COMMAND_NONE = 0,
+	COMMAND_INSTALL,
+	COMMAND_REINSTALL,
+};
+
 static void usage(const char *prog)
 {
-	printf("usage: %s [COMMANDS]\n", prog);
+	printf("usage: %s COMMAND [OPTIONS]\n", prog);
 	printf("\nCOMMANDS:\n");
-	printf("\t -r: restart the operation and ignore checkpoints\n");
-	printf("\t -i: start installtion\n");
+	printf("\t reinstall: restart the operation and ignore checkpoints\n");
+	printf("\t install: start installation\n");
+	printf("\nOPTIONS\n");
+	printf("\t -g: path to gcc 5 tar file (if not present it will be downloaded)\n");
+	printf("\t -c: path to cuda 9 run file (if not present it will be downloaded)\n");
+	printf("\t -d: path to install debian (if not present /opt/wheezy)\n");
 	exit(1);
 }
 
@@ -24,26 +35,37 @@ int main(int argc, char *argv[])
 	struct stepmgr stepmgr;
 	char logfile[64];
 	char token[10];
-	char reset = 0;
-	char install = 0;
+	char *gccpath = NULL, *cudapath = NULL, *debpath = NULL;
 	int opt;
+	enum command command = COMMAND_NONE;
 
-	while ((opt = getopt(argc, argv, "ri")) != -1)
+	while ((opt = getopt(argc, argv, "g:c:d:")) != -1)
 	{
 		switch(opt)
 		{
-			case 'r':
-				reset = 1;
+			case 'g':
+				gccpath = optarg;
 				break;
-			case 'i':
-				install = 1;
+			case 'c':
+				cudapath = optarg;
+				break;
+			case 'd':
+				debpath = optarg;
 				break;
 			default:
 				usage(argv[0]);
 		}
 	}
 
-	if ((!install && !reset) || (install && reset))
+	if (!argv[optind])
+		usage(argv[0]);
+
+	if (!strncmp("install", argv[optind], 7))
+		command = COMMAND_INSTALL;
+	else if (!strncmp("reinstall", argv[optind], 9))
+		command = COMMAND_REINSTALL;
+
+	if (command == COMMAND_NONE)
 		usage(argv[0]);
 
 	if (geteuid() != 0)
@@ -55,7 +77,8 @@ int main(int argc, char *argv[])
 	stepmgr_init(&stepmgr,
 							 steps,
 							 sizeof(steps) / sizeof(struct step),
-							 "/etc/gpgpusim_checkpoint", reset);
+							 "/etc/gpgpusim_checkpoint",
+							 command == COMMAND_REINSTALL);
 
 	rand_str(token, 10);
 	snprintf(logfile, 64, "/tmp/gpgpusim_install_%s.log", token);
