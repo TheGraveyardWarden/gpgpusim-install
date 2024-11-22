036fbede93f9cff612952386e87e95b586d18762
TheGraveyardWarden <alimanutd10r@gmail.com>
2024-11-22 19:58:06 +0330

    helper update

--- src/exec.h
+++ src/exec.h
@@ -4,7 +4,8 @@
 /*	Runs cmd until completion.
  *	Return value:
  *		0 on success
- *		< 0 child was terminated by a signal. return value is signal number * -1
+ *		-1 on err
+ *		< -1 child was terminated by a signal. return value is signal number * -1
  *		> 0 return value is cmd exit status
  */
 int exec(const char *cmd, ...);
