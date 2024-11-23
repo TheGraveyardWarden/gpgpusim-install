95b7e2c5a3746a61d9c932c5b9cc88455ce037ef
TheGraveyardWarden <ali.manutd10r@gmail.com>
2024-11-23 14:17:32 +0300

    updated install gpgpusim-install script

--- scripts/install-gpgpusim.sh
+++ scripts/install-gpgpusim.sh
@@ -6,5 +6,5 @@ DEBPATH=$1
 
 /bin/cp ./gpgpusim $DEBPATH/root -rfv
 run_wheezy "apt-get install -f"
-run_wheezy "apt-get install build-essential xutils-dev bison zlib1g-dev flex libglu1-mesa-dev doxygen graphviz libxi-dev libxmu-dev"
+run_wheezy "apt-get install build-essential xutils-dev bison zlib1g-dev flex libglu1-mesa-dev doxygen graphviz libxi-dev libxmu-dev -y --force-yes"
 run_wheezy "cd /root/gpgpusim; . setup_environment; make -j4"
