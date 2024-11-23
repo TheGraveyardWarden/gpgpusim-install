70ca0f06665d470015d85d086128c18b979a7aa6
TheGraveyardWarden <ali.manutd10r@gmail.com>
2024-11-23 14:57:31 +0300

    fix dpkg scripts

--- scripts/install-gcc-5.sh
+++ scripts/install-gcc-5.sh
@@ -11,13 +11,13 @@ WD=/root/build-gcc
 /bin/cp $GCCPATH $DEBPATH/$WD/gcc-5.debs.tar
 run_wheezy "/bin/tar -C $WD -xvf $WD/gcc-5.debs.tar"
 run_wheezy "/usr/bin/dpkg -P --force-all gcc-4.7-base"
-run_wheezy "yes | /usr/bin/dpkg -i $WD/gcc-5.debs/*.deb"
-run_wheezy "yes | /usr/bin/dpkg -i $WD/gcc-5.debs/g++-5.debs/*.deb"
+run_wheezy "DEBIAN_FRONTEND=noninteractive /usr/bin/dpkg --force-confdef -i $WD/gcc-5.debs/*.deb"
+run_wheezy "DEBIAN_FRONTEND=noninteractive /usr/bin/dpkg --force-confdef -i $WD/gcc-5.debs/g++-5.debs/*.deb"
 run_wheezy "/bin/ln -s /usr/bin/gcc-5 /usr/bin/gcc"
 run_wheezy "/bin/ln -s /usr/bin/g++-5 /usr/bin/g++"
 run_wheezy "/bin/ln -s /usr/bin/gcc /bin/gcc"
 run_wheezy "/bin/ln -s /usr/bin/g++ /bin/g++"
 run_wheezy '/bin/echo "export LD_PRELOAD=/lib/x86_64-linux-gnu/libc-2.18.so" >> /root/.bashrc'
 /bin/cp ./assets/cpp-dummy.deb ./assets/gcc-dummy.deb ./assets/g++-dummy.deb $DEBPATH/$WD
-run_wheezy "yes | /usr/bin/dpkg -i $WD/cpp-dummy.deb $WD/gcc-dummy.deb $WD/g++-dummy.deb"
+run_wheezy "DEBIAN_FRONTEND=noninteractive /usr/bin/dpkg --force-confdef -i $WD/cpp-dummy.deb $WD/gcc-dummy.deb $WD/g++-dummy.deb"
 run_wheezy "/bin/rm -rfv $WD"
