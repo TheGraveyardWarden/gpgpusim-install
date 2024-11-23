9035c58a161a6f7f51df7529b413d0ba63c4f7ab
TheGraveyardWarden <ali.manutd10r@gmail.com>
2024-11-23 14:13:44 +0300

    fix chroot path

--- scripts/run_wheezy.sh
+++ scripts/run_wheezy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 run_wheezy() {
-	/bin/chroot $DEBPATH /bin/sh -c ". /root/.bashrc; $1"
+	chroot $DEBPATH /bin/sh -c ". /root/.bashrc; $1"
 	CODE=$?
 	if [ $CODE -ne 0 ]; then
 		exit $CODE
